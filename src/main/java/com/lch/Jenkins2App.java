3046e4514c07ccf057fd54115c6866c90efeaed2
luomashijie <1832968964@qq.com>
2021-05-08 16:03:16 +0800

    测试jenkins

---
+++ src/main/java/com/lch/Jenkins2App.java
@@ -0,0 +1,11 @@
+package com.lch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkins2App {
+    public static void main(String[] args) {
+        SpringApplication.run(Jenkins2App.class, args);
+    }
+}
--- src/main/java/com/lch/controller/JenkinsController.java
+++ src/main/java/com/lch/controller/JenkinsController.java
@@ -9,6 +9,6 @@ public class JenkinsController {
 
     @GetMapping("jenkinsBuild")
     public String jenkinsBuild(){
-        return "jenkins构建成功 欧耶！！！";
+        return "jenkins构建成功 欧耶！！！终结者！！！！！哈哈哈啊哈哈哈哈！！！！！！";
     }
 }
